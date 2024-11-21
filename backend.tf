36e015891cd70c8d5711aeed6b57df76a831f0e6
manjugdr <153181318+manjugdr@users.noreply.github.com>
2024-11-21 18:41:25 +0530

    Create backend.tf

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-stfile"               # Your existing S3 bucket
+    key            = "my-vpc/terraform.tfstate"       # Path inside the bucket to store the state file
+    region         = "us-east-1"                      # Region of your S3 bucket
+    dynamodb_table = "terraform-lock-table"           # Name of your DynamoDB lock table
+    encrypt        = true                             # Encrypt state file at rest
+  }
+}
